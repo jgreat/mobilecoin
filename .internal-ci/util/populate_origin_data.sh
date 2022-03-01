bc614ad712174dfd270f46764e5072fad87a327c
Jason Greathouse <jgreat@jgreat.me>
2022-03-01 12:38:16 -0600

    ci 179

--- .internal-ci/util/populate_origin_data.sh
+++ .internal-ci/util/populate_origin_data.sh
@@ -22,6 +22,7 @@ is_set FOG_REPORT_URL
 
 mkdir -p sample_data/ledger
 mkdir -p sample_data/keys
+mkdir -p sample_data/fog_keys
 
 BIN_PATH=${BIN_PATH:-"target/release"}
 REAL_BIN_PATH=$(realpath "${BIN_PATH}")
@@ -34,10 +35,10 @@ pushd sample_data || exit 1
 
 "${REAL_BIN_PATH}/generate-sample-ledger" --txs 100
 
-# sample-keys --num 500 \
-#     --fog-report-url fog://fog.${NETWORK_FROM_BRANCH}.mobilecoin.com \
-#     --fog-authority-root /fog-authority-roots/${NETWORK_FROM_BRANCH}_ok_rsa_head.pem \
-#     --output-dir ./fog_keys
+"${REAL_BIN_PATH}/sample-keys" --num 500 \
+    --fog-report-url "${FOG_REPORT_URL}" \
+    --fog-authority-root "${FOG_AUTHORITY_ROOT_CA_CERT_PATH}" \
+    --output-dir ./fog_keys
 
 rm -f ./ledger/lock.mdb
 
